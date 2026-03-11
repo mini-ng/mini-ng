35ebf54bd7ec66f6cda753e37e68f1672a2802f3
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-11 17:31:54 +0100

    feat: added expression tokenizer

---
+++ packages/compiler/src/html_parser/expression_parser/tokens.ts
@@ -0,0 +1,24 @@
+export interface Token {
+    token: TokenType;
+    value: string | number;
+}
+
+export enum TokenType {
+    STRING,
+    NUMBER,
+    BOOL,
+    ADD,
+    SUB,
+    DIV,
+    MUL,
+    IDENTIFIER,
+    COMMA,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    PIPE,
+    DOT,
+    LEFT_SQUARE_BRACKET,
+    RIGHT_SQUARE_BRACKET,
+    TERNARY,
+    COLON,
+}
