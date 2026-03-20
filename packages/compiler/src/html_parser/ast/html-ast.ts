c3eebb7341e1648bd6fe20f51aed0a64bce3bd20
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-20 21:04:05 +0100

    fix: made BoundAttribute and Reference to return values

--- packages/compiler/src/html_parser/ast/html-ast.ts
+++ packages/compiler/src/html_parser/ast/html-ast.ts
@@ -1,18 +1,6 @@
-import {BoundText, Element, Template, Text, Comment, Node, AttributeNode} from "../nodes";
+import {BoundText, Element, Template, Text, Comment, AttributeNode} from "../nodes";
 import {ForLoopBlock, IfBlock, SwitchNode} from "../syntax-ast";
-import {TemplateStmt} from "../../template/view_generator";
-import ts from "typescript";
-import {sojourn} from "../sojourn/src";
-import {AttributeMarker} from "../../template/attribute_marker";
-import {
-    _generateTextInterpolateNode,
-    generateAdvanceNode,
-    generateElementEndNode,
-    generateElementStartNode, generateTextInterpolateNode,
-    generateTextNode
-} from "../../node-generation/node-generation";
 import {ASTWithSource} from "./ast";
-import {TemplateVisitor} from "../../visitor/template-visitor";
 
 export abstract class HtmlAstVisitor {
     abstract visitElement(expr: Element);
@@ -30,10 +18,25 @@ export abstract class HtmlAstVisitor {
     abstract visitVariable(expr: HtmlVariable);
 }
 
+export class DefaultHtmlAstVisitor extends HtmlAstVisitor {
+    visitElement(expr: Element) {}
+    visitText(expr: Text) {}
+    visitBoundText(expr: BoundText) {}
+    visitTemplate(expr: Template) {}
+    visitIfBlock(expr: IfBlock) {}
+    visitForLoopBlock(expr: ForLoopBlock) {}
+    visitSwitch(expr: SwitchNode) {}
+    visitComment(expr: Comment) {}
+    visitAttribute(expr: AttributeNode) {}
+    visitBoundAttribute(expr: BoundAttribute) {}
+    visitBoundEvent(expr: BoundEvent) {}
+    visitReference(expr: HtmlReference) {}
+    visitVariable(expr: HtmlVariable) {}
+}
+
 export abstract class HtmlAstExpression {
     abstract accept(visitor: HtmlAstVisitor)
 }
-
 
 export class BoundAttribute implements HtmlAstExpression {
     constructor(
@@ -45,7 +48,7 @@ export class BoundAttribute implements HtmlAstExpression {
     }
 
     accept(visitor: HtmlAstVisitor) {
-        visitor.visitBoundAttribute(this)
+        return visitor.visitBoundAttribute(this)
     }
 }
 
@@ -70,7 +73,7 @@ export class HtmlReference implements HtmlAstExpression {
     }
 
     accept(visitor: HtmlAstVisitor) {
-        visitor.visitReference(this)
+        return visitor.visitReference(this)
     }
 }
 
@@ -84,191 +87,4 @@ export class HtmlVariable implements HtmlAstExpression {
     accept(visitor: HtmlAstVisitor) {
         visitor.visitVariable(this)
     }
-}
-
-export class HtmlAstVisitorImpl extends HtmlAstVisitor {
-
-    private readonly stmts: ts.ExpressionStatement[] = [];
-    private readonly updateStmts: ts.ExpressionStatement[] = [];
-    private readonly templateStmts: TemplateStmt[] = []
-    private consts: ts.Expression[] = [];
-    private readonly outsideStatements: ts.Statement[] = [];
-    private slot = 0;
-    private index = 0;
-    private readonly implicitVariables = []
-    private ngContents = []
-    private readonly astVisitor = new TemplateVisitor();
-
-    generateView(html: string) {
-
-        const ast = sojourn(html);
-
-        ast.childNodes.forEach(childNode => {
-            childNode.accept(this);
-            this.incrementIndex();
-        })
-
-        return {
-            stmts: this.stmts,
-            updateStmts: this.updateStmts,
-            templateStmts: this.templateStmts,
-            consts: this.consts,
-            outsideStatements: this.outsideStatements,
-        };
-
-    }
-
-    visitBoundText(expr: BoundText) {
-
-        const index = this.index;
-
-        this.stmts.push(generateTextNode(index));
-
-        this.updateStmts.push(generateAdvanceNode(index.toString()));
-
-        this.updateStmts.push(_generateTextInterpolateNode(expr.value.ast.accept(this.astVisitor), this.implicitVariables))
-
-    }
-
-    visitComment(visitor: Comment) {
-
-    }
-
-    visitElement(el: Element) {
-
-        const attrsIndex = this.visitAllAttributes(el.attributeNodes)
-
-        el.inputs.forEach(input => {
-            input.accept(this)
-        })
-
-        el.outputs.forEach(output => {
-            output.accept(this)
-        })
-
-        el.references.forEach(ref => {
-            ref.accept(this)
-        })
-
-        const startNode = generateElementStartNode(this.index, el.name, attrsIndex)
-        this.stmts.push(startNode)
-
-        el.childNodes.forEach(childNode => {
-            this.incrementIndex()
-            childNode.accept(this)
-        });
-
-        const endNode = generateElementEndNode()
-        this.stmts.push(endNode)
-
-    }
-
-    visitForLoopBlock(visitor: ForLoopBlock) {
-    }
-
-    visitIfBlock(visitor: IfBlock) {
-    }
-
-    visitSwitch(visitor: SwitchNode) {
-    }
-
-    visitTemplate(visitor: Template) {
-    }
-
-    visitText(expr: Text) {
-        this.stmts.push(generateTextNode(this.index, expr.data))
-    }
-
-    visitAllAttributes(attributeNodes: AttributeNode[]) {
-
-        const constLength = this.consts.length
-
-        attributeNodes.forEach(node => {
-            node.accept(this)
-        });
-
-        if (this.consts.length > constLength) {
-            return this.consts.length - 1
-        }
-
-        return null
-
-    }
-
-    visitAttribute(expr: AttributeNode) {
-
-        // let attrIndex;
-
-        let attr_marker: AttributeMarker;
-        const tempConstsStmts = [];
-
-        const { name, value } = expr
-
-        switch (expr.name) {
-            case 'style': {
-                attr_marker = AttributeMarker.Styles;
-                break;
-            }
-
-            case 'class': {
-                attr_marker = AttributeMarker.Classes;
-                break;
-            }
-
-            default: {
-                break;
-            }
-        }
-
-        if (!attr_marker) {
-            tempConstsStmts.unshift(ts.factory.createStringLiteral(value))
-            tempConstsStmts.unshift(ts.factory.createStringLiteral(name))
-        } else {
-            tempConstsStmts.push(ts.factory.createNumericLiteral(attr_marker))
-        }
-
-        // if (!isTemplate) {
-
-            if (attr_marker & AttributeMarker.Classes) {
-                value.split(" ").forEach(attrValue => {
-                    tempConstsStmts.push(ts.factory.createStringLiteral(attrValue))
-                })
-            }
-
-            if (attr_marker & AttributeMarker.Styles) {
-                value.split(":").forEach(attrValue => {
-                    tempConstsStmts.push(ts.factory.createStringLiteral(attrValue))
-                })
-            }
-
-        // }
-
-        // here, push consts
-        if (tempConstsStmts.length > 0) {
-            this.consts.push(
-                ts.factory.createArrayLiteralExpression(
-                    tempConstsStmts
-                )
-            )
-            // attrIndex = this.consts.length - 1
-        }
-
-    }
-
-    incrementIndex() {
-        this.index++
-    }
-
-    visitBoundAttribute(expr: BoundAttribute) {
-    }
-
-    visitBoundEvent(expr: BoundEvent) {
-    }
-
-    visitReference(expr: HtmlReference) {
-    }
-
-    visitVariable(expr: HtmlVariable) {
-    }
-
 }
