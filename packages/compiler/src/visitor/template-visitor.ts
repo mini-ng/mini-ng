e6c30e50e904225d318b47f37d6d7b12de0226df
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-20 08:38:19 +0100

    fix: added logic for logical or, and, equality and comparison

--- packages/compiler/src/visitor/template-visitor.ts
+++ packages/compiler/src/visitor/template-visitor.ts
@@ -1,4 +1,4 @@
-import {AstVisitor, Identifier, Literal, Sequence, Unary} from "../html_parser/ast/ast-impl";
+import {AstExpression, AstVisitor, Identifier, Literal, Sequence, Unary} from "../html_parser/ast/ast-impl";
 import ts from "typescript";
 import {
     ArrayLiteralAST,
@@ -21,7 +21,98 @@ import {
 
 export class TemplateVisitor extends AstVisitor {
 
+    visitOperator(operator: string) {
+
+        let kind;
+
+        switch (operator) {
+
+            case "+": {
+                kind = ts.SyntaxKind.PlusToken;
+                break;
+            }
+
+            case "-": {
+                kind = ts.SyntaxKind.MinusToken;
+                break;
+            }
+
+            case "/": {
+                kind = ts.SyntaxKind.SlashToken;
+                break;
+            }
+
+            case "*": {
+                kind = ts.SyntaxKind.AsteriskToken;
+                break;
+            }
+
+            case "<": {
+                kind = ts.SyntaxKind.LessThanToken
+                break;
+            }
+
+            case "<<": {
+                kind = ts.SyntaxKind.LessThanLessThanToken
+                break;
+
+            }
+
+            case "<=": {
+                kind = ts.SyntaxKind.LessThanEqualsToken
+                break;
+            }
+
+            case ">": {
+                kind = ts.SyntaxKind.GreaterThanToken
+                break;
+            }
+
+            case ">>": {
+                kind = ts.SyntaxKind.GreaterThanGreaterThanToken
+                break;
+            }
+
+            case ">>>": {
+                kind = ts.SyntaxKind.GreaterThanGreaterThanGreaterThanToken
+                break;
+            }
+
+            case ">=": {
+                kind = ts.SyntaxKind.GreaterThanEqualsToken
+                break;
+            }
+
+            case "&": {
+                kind = ts.SyntaxKind.AmpersandToken;
+                break;
+            }
+
+            case "&&": {
+                kind = ts.SyntaxKind.AmpersandAmpersandToken;
+                break;
+            }
+
+            case "||": {
+                kind = ts.SyntaxKind.BarBarEqualsToken
+                break;
+            }
+
+        }
+
+        return kind;
+    }
+
     visitBinary(visitor: BinaryAST) {
+
+        const expr = this.visitOperator(visitor.operator);
+        const left = (visitor.left as AstExpression).accept(this);
+        const right = (visitor.right as AstExpression).accept(this);
+
+        return ts.factory.createBinaryExpression(
+            left, expr, right
+        )
+
     }
 
     visitCall(visitor: CallAST) {
@@ -34,6 +125,10 @@ export class TemplateVisitor extends AstVisitor {
     visitLiteral(expr: Literal) {
         if (expr.valueType === LiteralAstType.STRING) {
             return ts.factory.createStringLiteral(expr.name)
+        }
+
+        if (expr.valueType === LiteralAstType.NUMBER) {
+            return ts.factory.createNumericLiteral(expr.value)
         }
     }
 
