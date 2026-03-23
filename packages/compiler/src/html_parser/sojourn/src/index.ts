1cc97a3b1a9f228eaf255256f21da1b4e510ad0d
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-23 13:42:47 +0100

    fix: changed Tokenizer instance method

--- packages/compiler/src/html_parser/sojourn/src/index.ts
+++ packages/compiler/src/html_parser/sojourn/src/index.ts
@@ -4,7 +4,7 @@ import {Tokenizer} from "./tokenizer/tokenizer";
 
 export const sojourn = (html: string) => {
 
-    const tokens = new Tokenizer(html).start();
+    const tokens = Tokenizer.getInstance(html).start();
 
     const sojourner = new Sojourner(tokens)
     const nodes = sojourner.start()
