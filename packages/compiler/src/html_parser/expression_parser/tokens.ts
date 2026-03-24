b59660200a4a3682597e242acbad7bd1253b618e
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-24 19:21:10 +0100

    fix: added more tokens to support

--- packages/compiler/src/html_parser/expression_parser/tokens.ts
+++ packages/compiler/src/html_parser/expression_parser/tokens.ts
@@ -6,36 +6,62 @@ export interface Token {
 export enum TokenType {
     STRING,
     NUMBER,
-    TRUE,
-    FALSE,
-    ADD,
-    SUB,
-    DIV,
-    MUL,
+    TRUE, // true
+    FALSE, // false
+    ADD, // +
+    AdditionAssignment, // +=
+    SUB, // -
+    SubtractionAssignment, // -=
+    DIV, // /
+    MUL, // *
     IDENTIFIER,
-    COMMA,
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    PIPE,
-    DOT,
+    COMMA, // ,
+    LEFT_PAREN, // (
+    RIGHT_PAREN, // )
+    PIPE, // |
+    DOT, // .
     LEFT_SQUARE_BRACKET, // [
     RIGHT_SQUARE_BRACKET, // ]
-    TERNARY,
-    COLON,
-    LESS_THAN,
-    GREATER_THAN,
-    SHL,
-    SHR,
-    BIT_SHR,
-    LESS_EQUAL,
-    GREATER_EQUAL,
-    OR,
-    LOGICAL_AND,
-    AND,
+    TERNARY, // ?
+    COLON, // :
+    LESS_THAN, // <
+    GREATER_THAN, // >
+    SHL, // <<
+    SHR, // >>
+    BIT_SHR, // >>>
+    LESS_EQUAL, // =<
+    GREATER_EQUAL, // >=
+    LOGICAL_OR, // ||
+    LOGICAL_AND, // &&
+    AND, // &
     LBRACE, // {
     RBRACE, // }
     SPREAD, // ...
     KEYWORD,
-    STRICT_EQUAL,
-    STRICT_NOTEQUAL
+    EQUAL, // ==
+    STRICT_EQUAL, // ===
+    STRICT_NOTEQUAL, // !==
+    YIELD, // yield
+    STAR, // *
+    BITWISE_NOT, // ~
+    BITWISE_XOR_ASSIGN, // ^=
+    BITWISE_XOR, // ^
+    OPTIONAL_CHAINING, // ?.
+    NULLISH_COALESCING, // ??
+    NULLISH_COALESCING_ASSIGN, // ??=
+    LOGICAL_OR_ASSIGN, // ||=
+    LOGICAL_AND_ASSIGN, // &&=
+    BITWISE_OR_ASSIGN, // |=
+    BITWISE_AND_ASSIGN, // &=
+    UnsignedRightShiftAssignment, // >>>=
+    RightShiftAssignment, // >>=
+    LeftShiftAssignment, // <<=
+    RemainderAssignment, // %=
+    Remainder, // %
+    DivisionAssignment, // /=
+    MultiplicationAssignment, // *=
+    ExponentiationAssignment, // **=
+    Exponentiation, // **
+    Assignment, // =
+    LOGICAL_NOT, // !
 }
